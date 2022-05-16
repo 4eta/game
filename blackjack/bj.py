891cfa23b82947f1ae6e58293991cf6db16dcb3d
ikizawa <ikizawas@gmail.com>
2022-05-16 09:43:30 +0900

    done

--- blackjack/bj.py
+++ blackjack/bj.py
@@ -38,10 +38,10 @@ class Player():
         self.cards = [deck.draw_card(), deck.draw_card()]
     
     def card_open(self):
-        return f"dealer: {self.cards}"
+        return f"player: {self.cards}"
     
     def point_open(self):
-        return f"dealer: {count_card(self.cards)}"
+        return f"player: {count_card(self.cards)}"
     
     def hit(self):
         self.cards.append(deck.draw_card())
@@ -59,14 +59,86 @@ class Dealer():
     def card_close(self):
         return f"dealer: {self.cards[0]},[?,?]"
     
-    def point_open(self):
+    def point_close(self):
         return f"dealer: {str_point(self.cards[0])}"
     
     def hit(self):
         self.cards.append(deck.draw_card())
 
-for i in range(3):
-    p = Player()
-    print(p.cards)
-    print(p.card_open())
 
+def game():
+
+    player = Player()
+    print(player.card_open())
+    print(player.point_open())
+
+    dealer  = Dealer()
+    print(dealer.card_close())
+    print(dealer.point_close())
+
+    select = 0
+    while count_card(player.cards) <= 21:
+        time.sleep(2)
+        if count_card(player.cards) == 21:
+            print("Black Jack !!!")
+            return 1
+        select = input("do you draw a card?\nyes:1, no:0 ")
+
+        if select == "1":
+            player.hit()
+            print(player.card_open())
+            print(player.point_open())
+        else:
+            break
+    else:
+        print("BARST !!")
+        return 0
+
+    time.sleep(1)
+
+    print(dealer.card_open())
+    time.sleep(1)
+    print(dealer.point_open())
+
+    while count_card(dealer.cards) < 17:
+        time.sleep(1)
+        dealer.hit()
+        print(dealer.card_open())
+        time.sleep(1)
+        print(dealer.point_open())
+        if count_card(dealer.cards) > 21:
+            return 1
+    
+    time.sleep(1)
+
+    if count_card(dealer.cards) < count_card(player.cards):
+        return 1
+    elif count_card(dealer.cards) > count_card(player.cards):
+        return 0
+    else:
+        return -1
+
+start = 1
+win_lose = [0,0]
+
+deck = Deck()
+
+while len(deck.cards) > 10:
+    start = input("do you start game?\nyes:1, no:0 ")
+    if start != "1":
+        print("quit game")
+        break
+    print("let's start game")
+    result = game()
+    if result == 1:
+        win_lose[0] += 1
+        print("you win !!")
+        print("win:", win_lose[0], " lose:", win_lose[1])
+    elif result == 0:
+        win_lose[1] += 1
+        print("you lose !!")
+        print("win:", win_lose[0], " lose:", win_lose[1])
+    else:
+        print("draw !!")
+        print("win:", win_lose[0], " lose:", win_lose[1])
+print("there is no card")
